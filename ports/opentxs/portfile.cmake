852b2dd59a3a55ef4b5026717b3bce5a058ece65
Justus Ranvier <justus@opentransactions.org>
2023-05-17 13:29:16 -0500

    upgrade opentxs to 1.150.2

--- ports/opentxs/portfile.cmake
+++ ports/opentxs/portfile.cmake
@@ -1,5 +1,5 @@
 set(OPENTXS_REPO "https://github.com/Open-Transactions/opentxs")
-set(OPENTXS_COMMIT "8d8dda0ba3eb4837f96b4da9445ff4e6f33a790a")
+set(OPENTXS_COMMIT "930f165bb0cd47fe441d5db89878e86d609ac361")
 set(SOURCE_PATH "${DOWNLOADS}/opentxs.git")
 
 find_program(GIT git git.cmd NO_CMAKE_FIND_ROOT_PATH)
@@ -59,6 +59,24 @@ vcpkg_execute_in_download_mode(
   --recursive
 )
 
+if("qt5" IN_LIST FEATURES)
+  set(OPENTXS_USE_QT5 ON)
+  set(OPENTXS_QT_VERSION_MAJOR 5)
+endif()
+
+if("qt6" IN_LIST FEATURES)
+  set(OPENTXS_USE_QT6 ON)
+  set(OPENTXS_QT_VERSION_MAJOR 6)
+endif()
+
+if(OPENTXS_USE_QT5 AND OPENTXS_USE_QT6)
+  message(FATAL_ERROR "Only one version of Qt must be enabled")
+endif()
+
+if(OPENTXS_USE_QT5 OR OPENTXS_USE_QT6)
+  set(OPENTXS_USE_QT ON)
+endif()
+
 vcpkg_cmake_configure(
   SOURCE_PATH
   "${SOURCE_PATH}"
@@ -68,10 +86,11 @@ vcpkg_cmake_configure(
   -DOT_CRYPTO_SUPPORTED_KEY_RSA=ON
   -DOT_CASH_USING_LUCRE=OFF
   -DOT_SCRIPT_USING_CHAI=OFF
-  -DOT_WITH_QT=ON
-  -DOT_WITH_QML=ON
+  -DOT_WITH_QT=${OPENTXS_USE_QT}
+  -DOT_WITH_QML=${OPENTXS_USE_QT}
   -DOT_USE_VCPKG_TARGETS=ON
   -DOT_BOOST_JSON_HEADER_ONLY=OFF
+  -DQT_VERSION_MAJOR=${OPENTXS_QT_VERSION_MAJOR}
   OPTIONS_RELEASE
   -DOPENTXS_DEBUG_BUILD=OFF
   OPTIONS_DEBUG
