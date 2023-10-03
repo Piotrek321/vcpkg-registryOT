9a9be5980576cc4dd61b9c86761702d1d6ba5733
Justus Ranvier <justus@opentransactions.org>
2023-10-03 12:41:50 -0500

    upgrade opentxs to 1.178.3

--- ports/opentxs/portfile.cmake
+++ ports/opentxs/portfile.cmake
@@ -1,7 +1,7 @@
 set(OPENTXS_REPO "https://github.com/Open-Transactions/opentxs")
-set(OPENTXS_COMMIT "3ca58a961c4336bba441b44afc7d9065149189d0")
+set(OPENTXS_COMMIT "dbc6381df41104a8eadca57d073ab0313c7ee441")
 set(SOURCE_PATH "${DOWNLOADS}/opentxs.git")
-set(OT_VERSION_STRING "1.178.2")
+set(OT_VERSION_STRING "1.178.3")
 
 find_program(GIT git git.cmd NO_CMAKE_FIND_ROOT_PATH)
 
